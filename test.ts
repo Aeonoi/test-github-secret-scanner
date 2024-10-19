9c70116cbba86f47013f76adb01af8ce8517c49b
Aeonoi <dzhou527@gmail.com>
2024-10-19 14:33:39 -0400

    Add ts file

---
+++ test.ts
@@ -0,0 +1,4 @@
+const MONGO_URI_TEST =
+	"mongodb+srv://asd21313asd12313:asdasq23123asdascxzxc213123aqsd981293813@studyit.1231215asd123.mongodb.net/";
+const FAKE_API_KEY =
+	"123asd1231as11-623419-sad-12512wqse-asd-axdzx-c12-312359121896a";
